fd68420e7c384f1da0bbf9913ff33298aa000a1f
Joshua Tafoya <tafoyajd.91@gmail.com>
2023-05-22 10:16:46 -0700

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(fun = 'roller-skate') {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+const wrapp = function wrapAdjective() {
+
+}
+
+function wrapAdjective(highlight) {
+    return function (adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  }
